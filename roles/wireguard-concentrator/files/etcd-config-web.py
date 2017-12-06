3d7ceb2859f86e8ab10009ff4b43c89a6ec5f5a2
Kasalehlia <kasalehlia@clonejo.de>
2017-12-06 22:57:28 +0100

    add etcd-config-web service

---
+++ roles/wireguard-concentrator/files/etcd-config-web.py
@@ -0,0 +1,41 @@
+#!/srv/venv/bin/python3
+import asyncio
+import json
+import re
+
+from aioetcd3.client import client
+from aioetcd3.help import range_prefix
+
+from aiohttp import web
+
+etcd_client = client(endpoint="127.0.0.1:2379")
+
+def conv_val(raw):
+    try:
+        return json.loads(raw.decode())
+    except json.decoder.JSONDecodeError:
+        return raw.decode()
+
+async def config_for(client):
+    config = dict()
+    for key in ['default', client]:
+        raw = await etcd_client.range(key_range=range_prefix('/config/{}/'.format(key)))
+        config.update(dict([(a[0].decode().split('/', 3)[-1], conv_val(a[1])) for a in raw]))
+    return config
+
+async def web_config(request):
+    if 'pubkey' in request.query:
+        client = request.query.get('pubkey')
+        if client:
+            conf = json.dumps(await config_for(client))
+            return web.Response(content_type='application/json', text=conf)
+        else:
+            return web.Response(status=400)
+    else:
+        return web.Response(status=400)
+
+if __name__ == "__main__":
+    app = web.Application()
+    app.router.add_get('/config', web_config)
+    web.run_app(app, host=('::','0.0.0.0'), port=8080)
+
