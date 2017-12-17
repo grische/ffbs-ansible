21c7d8d11e6a19333645a435679c8448af5205dd
Jan Luebbe <jluebbe@debian.org>
2017-12-17 20:10:10 +0100

    fix routing test

--- tests/test_concentrator.py
+++ tests/test_concentrator.py
@@ -1,5 +1,6 @@
 testinfra_hosts = ['concentrator1', 'concentrator2', 'concentrator3']
 
 def test_routing_enabled(host):
-    assert host.sysctl("net.ipv4.ip_forward")
-    assert host.sysctl("net.ipv6.conf.all.forwarding=1")
+    with host.sudo():
+        assert host.sysctl("net.ipv4.ip_forward")
+        assert host.sysctl("net.ipv6.conf.all.forwarding")
