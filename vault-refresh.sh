a5bbff3852333dc610cae3412524883b6bbecac6
Jan Luebbe <jluebbe@debian.org>
2018-01-17 19:31:33 +0100

    add chris' key

---
+++ vault-refresh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+gpg2 --batch --use-agent --decrypt vault.gpg | \
+  gpg2 --batch --use-agent \
+  -r 59941A0EC8051123 \
+  -r A2AEB64DC1042B74 \
+  -r B294BC06C7552A7A \
+  -r BFCDA6A312FDA911 \
+  -r FAD89F12E404424E \
+  --encrypt > vault.gpg.new && mv vault.gpg.new vault.gpg
