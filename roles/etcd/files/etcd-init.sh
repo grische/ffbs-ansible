137590614e4d3693750852213763dcc509de95fa
Kasalehlia <kasalehlia@clonejo.de>
2018-01-22 21:46:48 +0100

    etcd: adapted initializer script to new ip ranges

--- roles/etcd/files/etcd-init.sh
+++ roles/etcd/files/etcd-init.sh
@@ -14,12 +14,12 @@ fi
 $PUT '/config/default/retry' 20
 $PUT '/config/default/wg_keepalive' 25
 $PUT '/config/default/mtu' 1420
-$PUT '/config/default/concentrators' '[{"address4": "172.16.0.1", "address6": "2001:bf7:380:1::1", "endpoint": "concentrator1", "pubkey": "LfwcsPGyih7XpzUHCEaCpoJWP0JzYOFv9ElfItlgAiM=", "ifname": "wg-c1"}, {"address4": "172.16.0.2", "address6": "2001:bf7:380:1::2", "endpoint": "concentrator2", "pubkey": "whT6vlbUGSBKzKJGlxhevJf/PU/Jvdi9P6oSzAZ0i0E=", "ifname": "wg-c2"}, {"address4": "172.16.0.3", "address6": "2001:bf7:380:1::3", "endpoint": "concentrator3", "pubkey": "Xzzv10CknsICmteoJCGwHkERtVhS6xkT6LzPCrDxURM=", "ifname": "wg-c3"}]'
+$PUT '/config/default/concentrators' '[{"address4": "10.0.0.1", "address6": "2001:bf7:381::1", "endpoint": "concentrator1:2300", "pubkey": "LfwcsPGyih7XpzUHCEaCpoJWP0JzYOFv9ElfItlgAiM=", "id": 1}, {"address4": "10.0.0.2", "address6": "2001:bf7:381::2", "endpoint": "concentrator2:2300", "pubkey": "whT6vlbUGSBKzKJGlxhevJf/PU/Jvdi9P6oSzAZ0i0E=", "id": 2}, {"address4": "10.0.0.1", "address6": "2001:bf7:381::3", "endpoint": "concentrator3:2300", "pubkey": "Xzzv10CknsICmteoJCGwHkERtVhS6xkT6LzPCrDxURM=", "id": 3}]'
 
 # insert test client (num 0x3fff)
 $PUT '/config/test/retry' 600
 $PUT '/config/test/id' 16383
-$PUT '/config/test/address4' '172.17.63.255'
+$PUT '/config/test/address4' '10.255.252.1'
 $PUT '/config/test/range4' '10.255.252.0/22'
 $PUT '/config/test/address6' '2001:bf7:381:3fff::1'
 $PUT '/config/test/range6' '2001:bf7:381:3fff::/64'
