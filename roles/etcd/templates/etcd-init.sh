f4173614f4bb4d34f361ce989eab0d4e394b762e
Jan Luebbe <jluebbe@debian.org>
2022-01-09 13:48:40 +0100

    etcd: use the external host name in the default node config endpoints

--- roles/etcd/templates/etcd-init.sh
+++ roles/etcd/templates/etcd-init.sh
@@ -21,7 +21,7 @@ fi
 $PUT '/config/default/retry' 20
 $PUT '/config/default/wg_keepalive' 25
 $PUT '/config/default/mtu' {{ wg_mtu }}
-$PUT '/config/default/concentrators' '[{% for host in groups['etcd_cluster'] %}{"address4": "{{hostvars[host]['wg_gateway_v4']}}", "address6": "{{hostvars[host]['wg_gateway_v6']}}", "endpoint": "{{host}}:10000", "pubkey": "{{hostvars[host]['wg_public_key']}}", "id": {{hostvars[host]['wg_backbone_id']}}}{% if not loop.last %}, {% endif %}{% endfor %}]'
+$PUT '/config/default/concentrators' '[{% for host in groups['etcd_cluster'] %}{"address4": "{{hostvars[host]['wg_gateway_v4']}}", "address6": "{{hostvars[host]['wg_gateway_v6']}}", "endpoint": "{{hostvars[host]['ansible_host']}}:10000", "pubkey": "{{hostvars[host]['wg_public_key']}}", "id": {{hostvars[host]['wg_backbone_id']}}}{% if not loop.last %}, {% endif %}{% endfor %}]'
 
 $PUT 'next_free_id' 1
 $PUT 'initialized' "$(date)"
