3ff8338047d8efbe43b794fa5c2db00732d0fc45
Grische <github@grische.xyz>
2024-11-08 19:10:16 +0100

    etcd: always initialise the default values

--- roles/etcd/templates/etcd-init.sh
+++ roles/etcd/templates/etcd-init.sh
@@ -10,6 +10,12 @@ if [[ $? -gt 0 ]]; then
     exit 0
 fi
 
+# insert/update default values
+$PUT '/config/default/retry' 20
+$PUT '/config/default/wg_keepalive' 25
+$PUT '/config/default/mtu' {{ wg_mtu }}
+$PUT '/config/default/concentrators' '[{% for host in groups['etcd_cluster'] %}{"address4": "{{hostvars[host]['wg_gateway_v4']}}", "address6": "{{hostvars[host]['wg_gateway_v6']}}", "endpoint": "{{hostvars[host]['ansible_host']}}:10000", "pubkey": "{{hostvars[host]['wg_public_key']}}", "id": {{hostvars[host]['wg_backbone_id']}}}{% if not loop.last %}, {% endif %}{% endfor %}]'
+
 # do not overwrite these values
 initialized=$($GET initialized)
 if [[ -n "$initialized" ]]; then
@@ -17,12 +23,5 @@ if [[ -n "$initialized" ]]; then
     exit 0
 fi
 
-# insert default values
-$PUT '/config/default/retry' 20
-$PUT '/config/default/wg_keepalive' 25
-$PUT '/config/default/mtu' {{ wg_mtu }}
-$PUT '/config/default/concentrators' '[{% for host in groups['etcd_cluster'] %}{"address4": "{{hostvars[host]['wg_gateway_v4']}}", "address6": "{{hostvars[host]['wg_gateway_v6']}}", "endpoint": "{{hostvars[host]['ansible_host']}}:10000", "pubkey": "{{hostvars[host]['wg_public_key']}}", "id": {{hostvars[host]['wg_backbone_id']}}}{% if not loop.last %}, {% endif %}{% endfor %}]'
-
 $PUT 'next_free_id' 1
 $PUT 'initialized' "$(date)"
-
